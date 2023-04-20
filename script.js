39270c5ab04da889cc1bbc9c24b374151d0c6d7f
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2023-04-20 08:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function fibonacci(num) {
 
-	if(num === 0)
-		return 0;
-	if(num === 1)
-		return 1;
+  let fib = [0, 1];
+  
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
 
-	return fibonacci(num-1) + fibonacci(num-2);
+  return fib;
 	
 }
 
